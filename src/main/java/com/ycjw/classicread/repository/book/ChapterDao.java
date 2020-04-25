7cb261c9e03d428764bee55be7eaa594b42a27d1
forroad <1712767833@qq.com>
2020-04-25 16:25:55 +0800

    1.优化了一些bug

--- src/main/java/com/ycjw/classicread/repository/book/ChapterDao.java
+++ src/main/java/com/ycjw/classicread/repository/book/ChapterDao.java
@@ -11,10 +11,10 @@ import java.util.List;
 @Repository
 public interface ChapterDao extends JpaRepository<Chapter,String> {
 
-    @Query(value = "select chapter.chapterName from Chapter chapter where chapter.bookId=?1")
+    @Query(value = "select chapter.chapterName from Chapter chapter where chapter.bookId=?1 order by chapter.chapterIndex")
     List<String> findAllNameByChapterId(String bookId);
 
-    @Query(value = "select chapter.chapterId from Chapter chapter where chapter.bookId=?1")
+    @Query(value = "select chapter.chapterId from Chapter chapter where chapter.bookId=?1 order by chapter.chapterIndex")
     List<String> findAllIdByBookId(String bookId);
 }
 
--- src/main/java/com/ycjw/classicread/service/impl/SpiderServiceImpl.java
+++ src/main/java/com/ycjw/classicread/service/impl/SpiderServiceImpl.java
@@ -8,17 +8,20 @@ import com.ycjw.classicread.service.SpiderSerice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.springframework.transaction.annotation.Transactional;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.pipeline.ConsolePipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.*;
 
 @Service
+@Transactional
 public class SpiderServiceImpl implements SpiderSerice {
     private BookDao bookDao;
     private ChapterDao chapterDao;
@@ -31,8 +34,9 @@ public class SpiderServiceImpl implements SpiderSerice {
 
     @Override
     public void crawl_book_data(String url) {
+        Random random = new Random();
         /**
-         * 解析章节内容
+         * 解析普通章节内容
          */
         class ChapterInfoProcessor implements PageProcessor{
             private Site site = Site.me().setRetryTimes(3).setSleepTime(2000);
@@ -51,6 +55,76 @@ public class SpiderServiceImpl implements SpiderSerice {
 
                 Chapter chapter = chapterDao.findById(chapterId).get();
                 chapter.setChapterInfo(chapterInfos);
+
+                chapterDao.save(chapter);
+            }
+
+            @Override
+            public Site getSite() {
+                return site;
+            }
+        }
+
+        /**
+         * 解析现代章节内容
+         */
+        class ChapterInfoProcessorXiandai implements PageProcessor{
+            private Site site = Site.me().setRetryTimes(3).setSleepTime(2000);
+            private String chapterId;
+
+
+            public ChapterInfoProcessorXiandai(String chapterId) {
+                this.chapterId = chapterId;
+            }
+
+            @Override
+            public void process(Page page) {
+                /**
+                 * 获取章节内容
+                 */
+                String chapterInfo = page.getHtml().xpath("//div[@class='panel-body']/p").get();
+                chapterInfo = chapterInfo.replace("<p>","");
+                chapterInfo = chapterInfo.replace("</p>","");
+                String[] chapterInfos = chapterInfo.split("<br><br>");
+
+
+                Chapter chapter = chapterDao.findById(chapterId).get();
+                chapter.setChapterInfo(Arrays.asList(chapterInfos));
+
+                chapterDao.save(chapter);
+            }
+
+            @Override
+            public Site getSite() {
+                return site;
+            }
+        }
+
+        /**
+         * 解析诗词章节内容
+         */
+        class ChapterInfoProcessorShici implements PageProcessor{
+            private Site site = Site.me().setRetryTimes(3).setSleepTime(2000);
+            private String chapterId;
+
+
+            public ChapterInfoProcessorShici(String chapterId) {
+                this.chapterId = chapterId;
+            }
+
+            @Override
+            public void process(Page page) {
+                /**
+                 * 获取章节内容
+                 */
+                String chapterInfo = page.getHtml().xpath("//div[@class='panel-body']").get();
+                chapterInfo = chapterInfo.replace("<div class=\"panel-body\" id=\"htmlContent\">","");
+                chapterInfo = chapterInfo.replace("</div>","");
+                String[] chapterInfos = chapterInfo.split("<br>");
+
+
+                Chapter chapter = chapterDao.findById(chapterId).get();
+                chapter.setChapterInfo(Arrays.asList(chapterInfos));
 
                 chapterDao.save(chapter);
             }
@@ -88,7 +162,7 @@ public class SpiderServiceImpl implements SpiderSerice {
                 book.setBookImgPath(bookImgUrl.get(0));
 
                 for(int i = 0;i < chapterUrls.size();i++){
-                    Chapter chapter = new Chapter(String.valueOf(System.currentTimeMillis()),bookId,chapterNames.get(i),new ArrayList<>(),i + 1);
+                    Chapter chapter = new Chapter(String.valueOf(System.currentTimeMillis() + random.nextInt(10000)),bookId,chapterNames.get(i),new ArrayList<>(),i + 1);
                     chapter = chapterDao.save(chapter);
                     book.addChapterId(chapter.getChapterId());
                     bookDao.save(book);
@@ -97,11 +171,26 @@ public class SpiderServiceImpl implements SpiderSerice {
                     new Thread(new Runnable() {
                         @Override
                         public void run() {
-                            Spider.create(new ChapterInfoProcessor(finalChapter.getChapterId()))
-                                    .addUrl(chapterUrls.get(finalI))
-                                    .addPipeline(new ConsolePipeline())
-                                    .thread(12)
-                                    .run();
+                            if(url.contains("www.guojiajiyi.cn/list/5")){
+                                Spider.create(new ChapterInfoProcessorXiandai(finalChapter.getChapterId()))
+                                        .addUrl(chapterUrls.get(finalI))
+                                        .addPipeline(new ConsolePipeline())
+                                        .thread(12)
+                                        .run();
+                            }else if(url.contains("www.guojiajiyi.cn/list/3")){
+                                Spider.create(new ChapterInfoProcessorShici(finalChapter.getChapterId()))
+                                        .addUrl(chapterUrls.get(finalI))
+                                        .addPipeline(new ConsolePipeline())
+                                        .thread(12)
+                                        .run();
+                            }else {
+                                Spider.create(new ChapterInfoProcessor(finalChapter.getChapterId()))
+                                        .addUrl(chapterUrls.get(finalI))
+                                        .addPipeline(new ConsolePipeline())
+                                        .thread(12)
+                                        .run();
+                            }
+
                         }
                     }).start();
                 }
@@ -129,7 +218,7 @@ public class SpiderServiceImpl implements SpiderSerice {
                     List<String> authors = page.getHtml().xpath("//table[@class='table']/tbody/tr/td[@class='text-muted']/text()").all();
                     List<String> bookUrls = page.getHtml().xpath("//table[@class='table']/tbody/tr/td/a[@class!='text-muted']/@abs:href").all();
                     for (int i = 0;i < bookNames.size();i++){
-                        Book book = new Book(String.valueOf(System.currentTimeMillis()),bookNames.get(i),authors.get(i),new ArrayList<String>(),0,bookUrls.get(i));
+                        Book book = new Book(String.valueOf(System.currentTimeMillis() + random.nextInt(10000)),bookNames.get(i),authors.get(i),new ArrayList<String>(),0,bookUrls.get(i));
                         book = bookDao.save(book);
                         Book finalBook = book;
                         int finalI = i;
@@ -156,19 +245,28 @@ public class SpiderServiceImpl implements SpiderSerice {
         Spider.create(new BookProcessor())
                 .addUrl(url)
                 .addPipeline(new ConsolePipeline())
-                .thread(1)
+                .thread(12)
                 .run();
     }
+
+
 
     @Override
     public void update_book() {
         List<Book> books = bookDao.findAll();
         for(Book book:books){
             if(book.getChapterIds().size() > 0){
-                Chapter chapter = chapterDao.findById(book.getChapterIds().get((0))).orElse(new Chapter());
-                List<String> chapter_info = chapter.getChapterInfo();
-                book.setFirstChapter(chapter_info.get(0));
-                bookDao.save(book);
+                try {
+                    Chapter chapter = chapterDao.findById(book.getChapterIds().get((0))).orElse(new Chapter());
+                    List<String> chapter_info = chapter.getChapterInfo();
+                    if(chapter_info.size() > 0){
+                        book.setFirstChapter(chapter_info.get(0));
+                        bookDao.save(book);
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                    continue;
+                }
             }
         }
         System.out.println("更新完成");
@@ -176,6 +274,52 @@ public class SpiderServiceImpl implements SpiderSerice {
 
     @Override
     public void test() {
-        
+        /**
+         * 解析章节内容
+         */
+        class ChapterInfoProcessor implements PageProcessor{
+            private Site site = Site.me().setRetryTimes(3).setSleepTime(2000);
+
+
+            @Override
+            public void process(Page page) {
+
+                writetxt(page.getHtml().toString());
+                /**
+                 * 获取章节内容
+                 */
+                String chapterInfo = page.getHtml().xpath("//div[@class='panel-body']").get();
+                chapterInfo = chapterInfo.replace("<div class=\"panel-body\" id=\"htmlContent\">","");
+                chapterInfo = chapterInfo.replace("</div>","");
+                String[] chapterInfos = chapterInfo.split("<br>");
+                System.out.println(Arrays.toString(chapterInfos));
+            }
+
+            @Override
+            public Site getSite() {
+                return site;
+            }
+        }
+
+
+        Spider.create(new ChapterInfoProcessor())
+                .addUrl("http://www.guojiajiyi.cn/w393/16009.html")
+                .addPipeline(new ConsolePipeline())
+                .thread(12)
+                .run();
+    }
+
+    private void writetxt(String text){
+        try {
+            /* 写入Txt文件 */
+            File writename = new File("C:\\Users\\17127\\Desktop\\log.txt"); // 相对路径，如果没有则要建立一个新的output。txt文件
+            writename.createNewFile(); // 创建新文件
+            BufferedWriter out = new BufferedWriter(new FileWriter(writename));
+            out.write(text); // \r\n即为换行
+            out.flush(); // 把缓存区内容压入文件
+            out.close(); // 最后记得关闭文件
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
